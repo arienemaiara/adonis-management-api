bf26cc42a872789940e20515c300b4a0aabcbad4
Ariene Ribeiro <aribeiro@img.com.br>
2020-06-21 19:19:16 -0300

    First commit

---
+++ .eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  env: {
+    browser: true,
+    commonjs: true,
+    es2020: true
+  },
+  extends: [
+    'standard'
+  ],
+  globals: {
+    use: true
+  },
+  parserOptions: {
+    ecmaVersion: 11
+  },
+  rules: {
+  }
+}
