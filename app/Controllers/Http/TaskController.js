a38df10ebb4e670dc17d42c182140f50aa960081
Ariene Ribeiro <aribeiro@img.com.br>
2020-06-21 23:06:18 -0300

    Adding routes validation

--- app/Controllers/Http/TaskController.js
+++ app/Controllers/Http/TaskController.js
@@ -5,70 +5,49 @@ const Task = use('App/Models/Task')
 class TaskController {
 
   async index ({ params }) {
-    try {
-      const tasks = await Task.query()
-        .where('project_id', params.projects_id)
-        .with('user')
-        .fetch()
+    const tasks = await Task.query()
+      .where('project_id', params.projects_id)
+      .with('user')
+      .fetch()
 
-      return tasks
-
-    } catch (error) {
-      
-    }
+    return tasks
   }
 
   async store ({ params, request }) {
-    try {
-      const data = request.only([
-        'user_id',
-        'title',
-        'description',
-        'due_date',
-        'file_id'
-      ])
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
 
-      const task = await Task.create({ ...data, project_id: params.projects_id })
+    const task = await Task.create({ ...data, project_id: params.projects_id })
 
-      return task
-
-    } catch (error) {
-      
-    }
+    return task
   }
 
   async show ({ params }) {
-    try {
-      
-      const task = await Task.findOrFail(params.id)
+    const task = await Task.findOrFail(params.id)
 
-      return task
-
-    } catch (error) {
-      
-    }
+    return task
   }
 
   async update ({ params, request }) {
-    try {
-      const task = await Task.findOrFail(params.id)
-      const data = request.only([
-        'user_id',
-        'title',
-        'description',
-        'due_date',
-        'file_id'
-      ])
+    const task = await Task.findOrFail(params.id)
+    const data = request.only([
+      'user_id',
+      'title',
+      'description',
+      'due_date',
+      'file_id'
+    ])
 
-      task.merge(data)
+    task.merge(data)
 
-      await task.save()
+    await task.save()
 
-      return task
-
-    } catch (error) {
-      
-    }
+    return task
   }
 
   async destroy ({ params }) {
--- start/routes.js
+++ start/routes.js
@@ -2,17 +2,36 @@
 
 const Route = use('Route')
 
-Route.post('users', 'UserController.create')
-Route.post('sessions', 'SessionController.create')
+Route.post('users', 'UserController.create').validator('User')
+Route.post('sessions', 'SessionController.create').validator('Session')
 
-Route.post('forgot_password', 'ForgotPasswordController.create')
-Route.put('forgot_password', 'ForgotPasswordController.update')
+Route.post('forgot_password', 'ForgotPasswordController.create').validator('ForgotPassword')
+Route.put('forgot_password', 'ForgotPasswordController.update').validator('ResetPassword')
 
 Route.get('files/:id', 'FileController.show')
 
 Route.group(() => {
     Route.post('files', 'FileController.store')
 
-    Route.resource('projects', 'ProjectController').apiOnly()
-    Route.resource('projects.tasks', 'TaskController').apiOnly()
+    Route.resource('projects', 'ProjectController')
+        .apiOnly()
+        .validator(new Map(
+            [
+                [
+                    ['projects.store'],
+                    ['Project']
+                ]
+            ]
+        ))
+
+    Route.resource('projects.tasks', 'TaskController')
+        .apiOnly()
+        .validator(new Map(
+            [
+                [
+                    ['projects.tasks.store'],
+                    ['Task']
+                ]
+            ]
+        ))
 }).middleware(['auth'])
