d11f1f05a9a07745db97c5baba684fadfaa93d42
Ariene Ribeiro <aribeiro@img.com.br>
2020-06-21 21:26:34 -0300

    Password reset

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -1,5 +1,6 @@
 'use strict'
 
+const moment = require('moment')
 const crypto = require('crypto')
 const Mail = use('Mail')
 
@@ -33,6 +34,36 @@ class ForgotPasswordController {
                 .send({ error: { message: 'Something went wrong. Check if you sent the right email.' } })
         }
         
+    }
+
+    async update ({ request, response }) {
+        try {
+
+            const { token, password } = request.all()
+
+            const user = await User.findByOrFail('token', token)
+
+            const tokenExpired = moment()
+                .subtract('2', 'days')
+                .isAfter(user.token_created_at)
+
+            if (tokenExpired) {
+                return response
+                    .status(401)
+                    .send({ error: { message: 'Token has expired.' } })
+            }
+
+            user.token = null
+            user.token_created_at = null
+            user.password = password
+
+            await user.save()
+            
+        } catch (error) {
+            return response
+                .status(error.status)
+                .send({ error: { message: 'Something went wrong.' } })
+        }
     }
 }
 
--- start/routes.js
+++ start/routes.js
@@ -6,3 +6,4 @@ Route.post('users', 'UserController.create')
 Route.post('sessions', 'SessionController.create')
 
 Route.post('forgot_password', 'ForgotPasswordController.create')
+Route.put('forgot_password', 'ForgotPasswordController.update')
